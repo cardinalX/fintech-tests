4c056849fcabe72f9556193b1170fd8f0d0ebf8d
cardinalX <mallebedev@gmail.com>
2022-01-15 21:00:50 +0300

    Настройка конфига playwright
    Добавлена переменная BASEURL.
    Изменены timeout`, добавлены репортеры.
    Viewport увеличен до FullHD

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,5 @@
 import { PlaywrightTestConfig, devices } from '@playwright/test';
+require('dotenv').config({ path: __dirname + '/.env' });
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -7,29 +8,26 @@ const config: PlaywrightTestConfig = {
 
   testDir: './specs',
 
-  /* Maximum time one test can run for. */
-  timeout: 30 * 1000,
+  /* Maximum time one test can run for. Default 30*/
+  timeout: 10 * 1000,
 
   expect: {
 
     /**
-     * Maximum time expect() should wait for the condition to be met.
+     * Maximum time expect() should wait for the condition to be met. Default 5000
      * For example in `await expect(locator).toHaveText();`
      */
-    timeout: 5000
+    timeout: 3000
   },
 
   /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
+  forbidOnly: Boolean(process.env.CI),
 
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
 
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
-
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [['allure-playwright'], ['dot'], ['html']],
 
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
@@ -38,10 +36,11 @@ const config: PlaywrightTestConfig = {
     actionTimeout: 0,
 
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://localhost:3000',
-
+    baseURL: process.env.BASEURL,
+    screenshot: 'only-on-failure',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    viewport: { width: 1920, height: 1080 },
   },
 
   /* Configure projects for major browsers */
