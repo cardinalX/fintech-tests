df9dc15ba8a1a0eba06a667fe40526ac804d5645
Максим Лебедев <malekslebedev@sberbank.ru>
2022-01-19 20:26:16 +0300

    Добавил последние тесты

--- specs/vacancy-page/vacancy-menu-vacancies.spec.ts
+++ specs/vacancy-page/vacancy-menu-vacancies.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from "@playwright/test";
+import { test, expect, Response } from "@playwright/test";
 import { VacancyPage } from "../../pages/vacancy-page";
 import listVacancies from "../../data/listVacancies.json";
 
@@ -10,28 +10,23 @@ test.describe('Страница вакансии Автоматизация те
     await test.step("Открытие страницы вакансии Автоматизация тестирования", async () => {
       await vacancyPage.goto();
     });
-
     await test.step("Открытие меню с вакансиями", async () => {
       await vacancyPage.buttonMoreVacanciesMenu.click();
     });
-
     await test.step(
       "Проверка, что появились элементы меню(вакансии)",
       async () => {
         await expect(vacancyPage.vacanciesMenuContainer).toBeVisible();
         await expect(vacancyPage.vacanciesMenuItems.first()).toBeVisible();
         await expect(vacancyPage.vacanciesMenuItems.last()).toBeVisible();
-        // +1 т.к. есть еще "Другие вакансии"
         expect(await vacancyPage.vacanciesMenuItems.count()).toEqual(
-          listVacancies.length + 1
+          listVacancies.length
         );
       }
     );
-
     await test.step("Закрытие меню с вакансиями", async () => {
       await vacancyPage.buttonCloseMenu.click();
     });
-
     await test.step(
       "Проверка, что элементов меню теперь не видно",
       async () => {
@@ -41,16 +36,61 @@ test.describe('Страница вакансии Автоматизация те
     );
   });
 
-  test('Открытие меню вакансий и переход на страницу вакансии "Автоматизация тестирования"', async ({ page }) => {
-    test.skip();
+  test("Открытие меню вакансий и переход по первой вакансии @x", async ({
+    page,
+  }) => {
+    const vacancyPage = new VacancyPage(page, vacancyPathname);
+    await test.step("Открытие страницы вакансии Автоматизация тестирования", async () => {
+      await vacancyPage.goto();
+    });
+
+    await test.step('Открытие меню по кнопке "Наши вакансии"', async () => {
+      await vacancyPage.buttonMoreVacanciesMenu.click();
+    });
+
+    let responsePage: Response;
+    await test.step("Нажимаем на первую вакансию из появившегося меню", async () => {
+      [responsePage] = await Promise.all([
+        vacancyPage.page.waitForNavigation(),
+        vacancyPage.vacanciesMenuItems.first().click(),
+      ]);
+    });
+    await test.step('Проверка, что после клика было перенаправление и 200-299 код ответа.', async () => {
+      expect(responsePage.ok()).toBeTruthy();
+    });
+
+    await test.step("Проверка видимости Заголовков для блоков с основной инфой", async () => {
+      await vacancyPage.shouldBeVisibleBlockHeaders();
+    });
+    await test.step("Проверка видимости блоков с текстом", async () => {
+      await vacancyPage.shouldBeVisibleTextBlocks();
+    });
+    await test.step("Проверка видимости Описания к задаче", async () => {
+      await expect(vacancyPage.taskDescription).toBeVisible();
+    });
+    await test.step("Проверка видимости Условия/кода задачи", async () => {
+      await expect(vacancyPage.taskCodeCondition).toBeVisible();
+    });
   });
 
-  test('Открытие меню вакансий и переход на страницу первой вакансии "DevOps-инженер"', async ({ page }) => {
-    test.skip();
-  });
+  test('Открытие меню вакансий и переход на страницу "Другие вакансии"', async ({
+    page, context
+  }) => {
+    const vacancyPage = new VacancyPage(page, vacancyPathname);
+    await test.step("Открытие страницы вакансии Автоматизация тестирования", async () => {
+      await vacancyPage.goto();
+    });
+    await vacancyPage.buttonMoreVacanciesMenu.click();
 
-  test('Открытие меню вакансий и переход на страницу "Другие вакансии"', async ({ page }) => {
-    test.skip();
+    const expectedURL =
+      "https://yandex.ru/jobs/services/finances/?utm_source=finpromoland";
+    const stepDesc = 'последний элемент в меню: "Другие вакансии"';
+    await vacancyPage.clickAndCheckNewPageResponse(
+      context,
+      expectedURL,
+      vacancyPage.vacanciesMenuItems.last(),
+      stepDesc
+    );
   });
 });
 
